d87486b5a0f485463772cdf73ed38a2c3b068753
Katja <47885564+katjalennartz@users.noreply.github.com>
2022-06-21 08:47:55 +0200

    Add files via upload

---
+++ upload/inc/languages/deutsch_du/application_ucp.lang.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Social Network 1.0 Main Language File
+ */
+
+
+// $l['application_ucp_permission'] = 'Steckbrief Benachrichtigungen';
+// $l['application_ucp_name'] = 'Steckbrief im UCP';
+// $l['application_ucp_menu'] = 'Steckbrief Einstellungen';
+// $l['application_ucp_overview'] = 'Steckbrieffelder Übersicht';
+// $l['application_ucp_createquesttype'] = 'Steckbrieffeld erstellen';
+
+
+?>
