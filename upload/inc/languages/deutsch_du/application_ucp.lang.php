1c2224691fddccb68692754f47a00231357d569e
Katja <47885564+katjalennartz@users.noreply.github.com>
2022-07-21 12:53:13 +0200

    Add files via upload

--- upload/inc/languages/deutsch_du/application_ucp.lang.php
+++ upload/inc/languages/deutsch_du/application_ucp.lang.php
@@ -3,6 +3,22 @@
  * Social Network 1.0 Main Language File
  */
 
+$l['application_ucp_usernav'] = 'Steckbrief';
+$l['application_ucp_mandatory'] = '*';
+$l['application_ucp_extbtn'] = 'Frist Verlängern';
+$l['application_ucp_save'] = 'Speichern & zur Kontrolle';
+$l['application_ucp_readybtn'] = 'Speichern & zur Kontrolle';
+
+$l['application_ucp_export'] = 'Steckbrief exportieren';
+
+$l['application_ucp_wobgroups'] = 'Primäre Benutzergruppe *';
+$l['application_ucp_wobgroups2'] = 'Sekundäre Benutzergruppe';
+
+$l['application_ucp_affected'] = 'Betroffene Charaktere:';
+
+//My Alert
+$l['myalerts_setting_application_ucp_affected'] = 'Steckbriefe: Benachrichtigung wenn dich ein Steckbrief betrifft.';
+$l['application_ucp_affected'] = '{1} hat einen Steckbrief gepostet der dich betrifft. Bitte gib dein Okay.';
 
 // $l['application_ucp_permission'] = 'Steckbrief Benachrichtigungen';
 // $l['application_ucp_name'] = 'Steckbrief im UCP';
