2fa90ef05ee70f2541080cff322106b55ba26026
Katja <47885564+katjalennartz@users.noreply.github.com>
2022-07-21 17:25:01 +0200

    Update application_ucp.lang.php

--- upload/inc/languages/deutsch_du/application_ucp.lang.php
+++ upload/inc/languages/deutsch_du/application_ucp.lang.php
@@ -6,7 +6,7 @@
 $l['application_ucp_usernav'] = 'Steckbrief';
 $l['application_ucp_mandatory'] = '*';
 $l['application_ucp_extbtn'] = 'Frist Verlängern';
-$l['application_ucp_save'] = 'Speichern & zur Kontrolle';
+$l['application_ucp_save'] = 'Speichern';
 $l['application_ucp_readybtn'] = 'Speichern & zur Kontrolle';
 
 $l['application_ucp_export'] = 'Steckbrief exportieren';
@@ -20,12 +20,6 @@ $l['application_ucp_affected'] = 'Betroffene Charaktere:';
 //My Alert
 $l['myalerts_setting_application_ucp_affected'] = 'Steckbriefe: Benachrichtigung wenn dich ein Steckbrief betrifft.';
 $l['application_ucp_affected'] = '{1} hat einen Steckbrief gepostet der dich betrifft. Bitte gib dein Okay.';
-
-// $l['application_ucp_permission'] = 'Steckbrief Benachrichtigungen';
-// $l['application_ucp_name'] = 'Steckbrief im UCP';
-// $l['application_ucp_menu'] = 'Steckbrief Einstellungen';
-// $l['application_ucp_overview'] = 'Steckbrieffelder Übersicht';
-// $l['application_ucp_createquesttype'] = 'Steckbrieffeld erstellen';
 
 
 ?>
