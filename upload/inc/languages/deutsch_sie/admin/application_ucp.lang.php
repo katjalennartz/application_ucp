79d1b18456145e6cf68f5e8c2fd45825f792dfdc
Katja <47885564+katjalennartz@users.noreply.github.com>
2022-07-21 13:27:07 +0200

    Add files via upload

---
+++ upload/inc/languages/deutsch_sie/admin/application_ucp.lang.php
@@ -0,0 +1,105 @@
+<?php
+//Infos
+$l['application_ucp_info_name'] = 'Bewerbung im UCP von Risuena';
+$l['application_ucp_info_descr'] = 'Möglichkeit im UCP einen eigenen Ausfüllbereich für Steckbriefe/Charakterinfos zu haben';
+
+
+$l['application_ucp_permission'] = 'Steckbrief Benachrichtigungen';
+$l['application_ucp_name'] = 'Steckbrief im UCP';
+$l['application_ucp_menu'] = 'Steckbrief Einstellungen';
+
+$l['application_ucp_overview'] = 'Übersicht';
+$l['application_ucp_overview_appl'] = 'Eine Übersicht aller Steckbrieffelder';
+$l['application_ucp_overview_sort'] = 'Sortierung';
+$l['application_ucp_overview_opt'] = 'Optionen';
+
+$l['application_ucp_delete'] = 'löschen';
+$l['application_ucp_deactivate'] = 'deaktiviert';
+$l['application_ucp_activate'] = 'aktiviert';
+
+$l['application_ucp_delete_ask'] = "Soll das Feld wirklich gelöscht werden? Achtung, auch die Inhalte der User werden gelöscht! Sonst das Feld einfach deaktivieren.";
+$l['application_ucp_deactivate_ask'] = "Soll das Feld wirklich deaktiviert werden? Die Inhalte werden nicht gelöscht und die Daten werden behalten.";
+$l['application_ucp_activate_ask'] = "Soll das Feld wirklich aktiviert werden? Die vorher ausgeblendeten Inhalte werden wieder angezeigt.";
+
+$l['application_ucp_manageusers'] = 'Steckbriefe verwalten';
+$l['application_ucp_manageusers_dscr'] = 'Steckbriefe der User verwalten und bearbeiten';
+$l['application_ucp_manageusers_all'] = 'Übersicht aller User und die Möglichkeit ihre Steckbriefe zu verwalten.';
+
+$l['application_ucp_manageusers_manage'] = 'Verwalten';
+$l['application_ucp_manageusers_application'] = 'Steckbrief verwalten';
+
+
+$l['application_ucp_edituser'] = 'Steckbrief bearbeiten.';
+
+
+$l['application_ucp_createfieldtype'] = 'Steckbrieffeld erstellen';
+$l['application_ucp_createfieldtype_dscr'] = 'Hier kannst du ein Steckbrieffeld anlegen und alle Einstellungen vornehmen.';
+$l['application_ucp_editfieldtype'] = 'Steckbrieffeld editieren';
+$l['application_ucp_editfieldtype_dscr'] = 'Hier kannst du ein Steckbrieffeld editiere und alle Einstellungen ändern.';
+
+$l['application_ucp_formname'] = 'Steckbrieffeld erstellen';
+$l['application_ucp_formname_edit'] = 'Steckbrieffeld editieren';
+
+$l['application_ucp_add_name'] = 'Name des Felds';
+$l['application_ucp_add_name_descr'] = 'Die Bezeichnung für das Feld, keine Sonderzeichen, keine Leertasten. Dient als Identifikator.';
+
+$l['application_ucp_add_fieldlabel'] = 'Label des Felds';
+$l['application_ucp_add_fieldlabel_descr'] = 'Was soll vor dem Feld stehen? Also z.B. Vorname';
+
+$l['application_ucp_add_fieldtyp'] = 'Typ des Felds';
+$l['application_ucp_add_fieldtyp_descr'] = 'Welchen Typ soll das Feld haben?';
+
+$l['application_ucp_add_fieldoptions'] = 'Auswahloptionen des Felds';
+$l['application_ucp_add_fieldoptions_descr'] = 'Die bei Select, Select-Multiple, Checkbox und Radiobuttons möglichen Antwortoptionen. Sonst leer lassen.<br/>Mit \',\' getrennt. Zum Beispiel: Ravenclaw, Hufflepuff, Gryffindor, Slytherin';
+
+$l['application_ucp_add_fieldeditable'] = 'Editierbar?';
+$l['application_ucp_add_fieldeditable_descr'] = 'Ist das Feld auch nach der Annahme des Steckbriefs editierbar?';
+
+$l['application_ucp_add_fieldmandatory'] = 'Pflichtfeld?';
+$l['application_ucp_add_fieldmandatory_descr'] = 'Ist das Feld ein Pflichtfeld?';
+
+$l['application_ucp_add_fielddependency'] = 'Abhängigkeit?';
+$l['application_ucp_add_fielddependency_descr'] = 'Ist das Feld nur sichtbar, wenn es von dem Wert eines anderen abhängig ist? Wenn ja, dieses hier auswählen.';
+
+$l['application_ucp_add_fielddependencyval'] = 'Abhängigkeitswert?';
+$l['application_ucp_add_fielddependencyval_descr'] = 'Von welcher auswählbaren Option soll das Feld abhängigkeit? Z.B "Hogwarts".<br/>Leer lassen wenn keine Abhängigkeit.<br/>
+ <b>Achtung:</b> genauso geschrieben, wie in den Auswahloptionen des Felds, welches bei Abhängigkeit gewählt ist!';
+
+$l['application_ucp_add_fieldpostbit'] = 'Anzeige Postbit?';
+$l['application_ucp_add_fieldpostbit_descr'] = 'Soll das Feld im Postbit anzeigbar sein?';
+
+$l['application_ucp_add_fieldprofile'] = 'Anzeige Profil?';
+$l['application_ucp_add_fieldprofile_descr'] = 'Soll das Feld im Profil anzeigbar sein?';
+
+$l['application_ucp_add_fieldtemplate'] = 'Vorlage/Template?';
+$l['application_ucp_add_fieldtemplate_descr'] = 'Hier könnt ihr ein Template für das Feld erstellen. Z.B. den Code für eine Timeline.';
+
+$l['application_ucp_add_fieldhtml'] = 'HTML erlaubt?';
+$l['application_ucp_add_fieldhtml_descr'] = 'Soll HTML in dem Feld benutzt werden dürfen?';
+
+$l['application_ucp_add_fieldmybb'] = 'MyBB-Code erlaubt?';
+$l['application_ucp_add_fieldmybb_descr'] = 'Soll MyBB-Code in dem Feld benutzt werden dürfen?';
+
+$l['application_ucp_add_fieldimg'] = 'IMG-Tag erlaubt?';
+$l['application_ucp_add_fieldimg_descr'] = 'Dürfen Bilder via IMG Tag in dem Feld eingebunden werden?';
+
+$l['application_ucp_add_fieldvideo'] = 'Videos erlaubt?';
+$l['application_ucp_add_fieldvideo_descr'] = 'Dürfen Videos in dem Feld eingebunden werden?';
+
+$l['application_ucp_add_fieldsort'] = 'Darstellungsreihenfolge?';
+$l['application_ucp_add_fieldsort_descr'] = 'An welcher Position soll das Feld dargestellt werden?';
+
+$l['application_ucp_save'] = 'Speichern';
+
+// Errors
+$l['application_ucp_err_name'] = 'Bitte einen Namen angeben.';
+$l['application_ucp_err_name_sonder'] = 'Der Name darf keine Sonderzeichen oder Leertasten enthalten.';
+$l['application_ucp_err_label'] = 'Bitte ein Label angeben.';
+$l['application_ucp_err_fieldtyp'] = 'Bitte einen Feldtypen auswählen.';
+$l['application_ucp_err_fieldoptions'] = 'Du hast einen Feldtypen ausgewählt, der Optionen erfordert. Bitte ausfüllen.';
+$l['application_ucp_err_dependency_value_empty'] = 'Du hast einen Abhängigkeit ausgewählt, aber nicht angegeben von welchem Wert dein erstelltes Abhängig sein soll.';
+$l['application_ucp_err_dependency_value_wrong'] = 'Für dein ausewähltes Feld gibt es die angegeben Option nicht. Vertippt? Bitte auch Groß- und Kleinschreibung beachten.';
+
+$l['application_ucp_err_delete'] = "Das Feld konnte nicht gelöscht werden.";
+
+$l['application_ucp_success'] = 'Erfolgreich gespeichert.';
