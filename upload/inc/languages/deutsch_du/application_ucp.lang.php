7337b0d2ab0487c0595d513bb194f3655a0a559c
Katja <47885564+katjalennartz@users.noreply.github.com>
2022-07-21 17:39:32 +0200

    Add files via upload

--- upload/inc/languages/deutsch_du/application_ucp.lang.php
+++ upload/inc/languages/deutsch_du/application_ucp.lang.php
@@ -15,11 +15,17 @@ $l['application_ucp_wobgroups'] = 'Primäre Benutzergruppe *';
 $l['application_ucp_wobgroups2'] = 'Sekundäre Benutzergruppe';
 $l['application_ucp_wobbtn'] = 'WOB';
 
-$l['application_ucp_affected'] = 'Betroffene Charaktere:';
+$l['application_ucp_affected_label'] = 'Betroffene Charaktere:';
 
 //My Alert
 $l['myalerts_setting_application_ucp_affected'] = 'Steckbriefe: Benachrichtigung wenn dich ein Steckbrief betrifft.';
 $l['application_ucp_affected'] = '{1} hat einen Steckbrief gepostet der dich betrifft. Bitte gib dein Okay.';
+
+// $l['application_ucp_permission'] = 'Steckbrief Benachrichtigungen';
+// $l['application_ucp_name'] = 'Steckbrief im UCP';
+// $l['application_ucp_menu'] = 'Steckbrief Einstellungen';
+// $l['application_ucp_overview'] = 'Steckbrieffelder Übersicht';
+// $l['application_ucp_createquesttype'] = 'Steckbrieffeld erstellen';
 
 
 ?>
