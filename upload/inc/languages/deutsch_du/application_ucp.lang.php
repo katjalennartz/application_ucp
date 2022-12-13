9dd2e216da269c1be4bafe33afa5d9cb1490c53d
Katja <47885564+katjalennartz@users.noreply.github.com>
2022-12-13 17:30:17 +0100

    Add files via upload

--- upload/inc/languages/deutsch_du/application_ucp.lang.php
+++ upload/inc/languages/deutsch_du/application_ucp.lang.php
@@ -4,7 +4,25 @@
  * by risuena
  */
 
+//Templates 
+$l['application_ucp_temps_title'] = 'Steckbriefübersicht';
+$l['application_ucp_temps_ready_applications'] = 'fertige Steckbriefe';
+$l['application_ucp_temps_in_correction_user'] = 'Steckbriefe die vom User korrigiert werden müssen';
+
+$l['application_ucp_temps_charakter'] = 'Charakter';
+$l['application_ucp_temps_application'] = 'Steckbrief';
+$l['application_ucp_temps_wip'] = 'In Bearbeitung?';
+$l['application_ucp_temps_frist'] = 'Fristende?';
+$l['application_ucp_temps_registeredsince'] = 'Registriert seit?';
+$l['application_ucp_temps_lastactive'] = 'Letzte Aktivität?';
+$l['application_ucp_temps_notready'] = 'noch nicht eingereichte Steckbriefe';
+$l['application_ucp_temps_profilelink'] = 'Link zum Profil';
+$l['application_ucp_temps_responsible'] = 'Verantwortlicher angefordert?';
+$l['application_ucp_temps_correctdate'] = 'eingereicht/korrigiert am';
+
+
 $l['application_ucp_usernav'] = 'Steckbrief';
+$l['application_ucp_fillapplication'] = 'Steckbrief ausfüllen';
 $l['application_ucp_mandatory'] = '*';
 $l['application_ucp_wanted'] = 'Ist der Charakter ein Gesuch?';
 $l['application_ucp_wanted_url'] = 'URL zum Gesuch';
@@ -19,6 +37,8 @@ $l['application_ucp_thread_wantedurltitle'] = "Charakter ist ein Gesuch";
 
 $l['application_ucp_wobgroups'] = 'Primäre Benutzergruppe *';
 $l['application_ucp_wobgroups2'] = 'Sekundäre Benutzergruppe';
+$l['application_ucp_nonewobgroups2'] = 'Keine sekundäre Gruppe';
+
 $l['application_ucp_wobbtn'] = 'WOB';
 
 $l['application_ucp_affected_label'] = 'Betroffene Charaktere:';
