122099b90257e2e24672ffc768f7edfd549e4d76
Katja <47885564+katjalennartz@users.noreply.github.com>
2024-05-27 16:10:12 +0200

    Update application_ucp.lang.php

--- upload/inc/languages/english/application_ucp.lang.php
+++ upload/inc/languages/english/application_ucp.lang.php
@@ -1,69 +1,68 @@
 <?php
 /**
- * Steckbriefe im UCP  1.0 Main Language File
+ * Profiles in UCP  1.0 Main Language File
  * by risuena
  */
 
 //Templates 
-$l['application_ucp_temps_title'] = 'overview personal descriptions';
-$l['application_ucp_temps_ready_applications'] = 'finished fact sheet';
-$l['application_ucp_temps_in_correction_user'] = 'personal description that waiting of corrections from user';
+$l['application_ucp_temps_title'] = 'Profile Overview';
+$l['application_ucp_temps_ready_applications'] = 'completed profiles';
+$l['application_ucp_temps_in_correction_user'] = 'profiles that need to be corrected by the user';
 
-$l['application_ucp_temps_charakter'] = 'character';
-$l['application_ucp_temps_application'] = 'personal description';
-$l['application_ucp_temps_wip'] = 'in progress?';
-$l['application_ucp_temps_frist'] = 'deadline?';
-$l['application_ucp_temps_registeredsince'] = 'registered since?';
-$l['application_ucp_temps_lastactive'] = 'last activity?';
-$l['application_ucp_temps_notready'] = 'personal description (not yet published)';
-$l['application_ucp_temps_profilelink'] = 'link to profile';
-$l['application_ucp_temps_responsible'] = 'person responsible requested?';
-$l['application_ucp_temps_correctdate'] = 'published:';
+$l['application_ucp_temps_charakter'] = 'Character';
+$l['application_ucp_temps_application'] = 'Profile';
+$l['application_ucp_temps_wip'] = 'In progress?';
+$l['application_ucp_temps_frist'] = 'Deadline?';
+$l['application_ucp_temps_registeredsince'] = 'Registered since?';
+$l['application_ucp_temps_lastactive'] = 'Last active?';
+$l['application_ucp_temps_notready'] = 'not yet submitted profiles';
+$l['application_ucp_temps_profilelink'] = 'Link to profile';
+$l['application_ucp_temps_responsible'] = 'Responsible requested?';
+$l['application_ucp_temps_correctdate'] = 'submitted/corrected on';
 
 //UCP
-$l['application_ucp_usernav'] = 'personal description';
-$l['application_ucp_fillapplication'] = 'fill personal description';
+$l['application_ucp_usernav'] = 'Profile';
+$l['application_ucp_fillapplication'] = 'Fill out profile';
 $l['application_ucp_mandatory'] = '*';
-$l['application_ucp_trigger'] = 'specify triggers for profile ';
-$l['application_ucp_wanted'] = 'is this a requested character?';
-$l['application_ucp_wanted_url'] = 'title/link/name to Request';
-$l['application_ucp_affected_ucp'] = 'affected characters';
-$l['application_ucp_extbtn'] = 'expand deadline';
-$l['application_ucp_save'] = 'save';
-$l['application_ucp_readybtn'] = 'save & publish';
-$l['application_ucp_saveerror'] = "you can't publish your personal description if you did't fill all requested fields. Please add affected characters again, if you've added some.";
-$l['application_ucp_export'] = 'export personal description';
-$l['application_ucp_infoheader'] = "you're registred since {1}. you expand {2} times. Your deadline is:  {3}.";
-$l['application_ucp_correction'] = "you've published your personal description";
+$l['application_ucp_trigger'] = 'Enter possible triggers for the profile here.';
+$l['application_ucp_wanted'] = 'Is the character a request?';
+$l['application_ucp_wanted_url'] = 'Title of the request, or name';
+$l['application_ucp_affected_ucp'] = 'Affected members';
+$l['application_ucp_extbtn'] = 'Extend Deadline';
+$l['application_ucp_save'] = 'Save';
+$l['application_ucp_readybtn'] = 'Save & Submit for Review';
+$l['application_ucp_saveerror'] = 'You cannot submit your profile until all mandatory fields are filled out. If you had entered new affected members, you need to re-enter them before saving.';
+$l['application_ucp_export'] = 'Export Profile';
+$l['application_ucp_infoheader'] = 'You have been registered since {1}. You have extended {2} and have time until {3}.';
+$l['application_ucp_correction'] = 'Your profile has been submitted.';
 
 //Memberprofile
-$l['application_ucp_profile_trigger'] = "Trigger alert:";
+$l['application_ucp_profile_trigger'] = "Trigger Warning:";
 
 //INDEX
-$l['application_ucp_index_extinfo'] = "you expanded {1} times.";
-$l['application_ucp_index_extinfo_deadline'] = "you've got time until {1} to finish your personal description.{2}";
-$l['application_ucp_index_nomod'] = "no moderator has take over yet.";
-$l['application_ucp_index_token'] = "your personal description will be checked by {1}.";
-$l['application_ucp_index_correction'] = "your personal description is checked by {1}. you've got time for improvments until {2}.";
-$l['application_ucp_index_mod_steckialert'] =  "{1} has published the personal description. <a href=\"misc.php?action=take_application&uid={2}\">take responsiblity</a>";
-$l['application_ucp_index_mod_steckialert_modturn'] =  "you've take responsibility for {1} personal description.";
-$l['application_ucp_index_mod_steckialert_userturn'] =  "you've take responsibility for {1}. The character has to make improvements.";
-$l['application_ucp_index_mod_steckialert_userhascorrected'] =  "you've take responsibility for {1}. The character has to made improvements.";
+$l['application_ucp_index_extinfo'] = "You have extended {1} times.";
+$l['application_ucp_index_extinfo_deadline'] = "You have until {1} to complete your profile.{2}";
+$l['application_ucp_index_nomod'] = "Your profile has not yet been taken over by any moderator.";
+$l['application_ucp_index_token'] = "Your profile has been taken over by {1}.";
+$l['application_ucp_index_correction'] = "Your profile was corrected by {1}. You have until {2} to make the corrections.";
+$l['application_ucp_index_mod_steckialert'] =  "{1} has completed the profile. <a href=\"misc.php?action=take_application&uid={2}\">Take over correction</a>";
+$l['application_ucp_index_mod_steckialert_modturn'] =  "You have taken over the profile of {1}. You still need to correct it.";
+$l['application_ucp_index_mod_steckialert_userturn'] =  "You have taken over the profile of {1}. The user has not yet corrected it.";
+$l['application_ucp_index_mod_steckialert_userhascorrected'] =  "You have taken over the profile of {1}. The user has corrected it and you need to review it.";
 
 //WOB - Thread
-$l['application_ucp_thread_wantedurltitle'] = "This Character is a requested character";
-$l['application_ucp_thread_nowanted'] = "not a requested character";
-$l['application_ucp_thread_trigger'] = "Trigger alert:";
-$l['application_ucp_wobgroups'] = 'primary usergroup *';
-$l['application_ucp_wobgroups2'] = 'secondary usergroup';
-$l['application_ucp_nonewobgroups2'] = 'no secondary usergroup';
-$l['application_ucp_responsible'] ="<span class=\"aucp_respmod\">{1} has take over</span>";
-$l['application_ucp_noresponsible'] ="<span class=\"aucp_respmod\">no one is responsible at the moment</span>";
-$l['application_ucp_wobbtn'] = 'welcome on board';
-$l['application_ucp_affected_label'] = 'affected characters';
-$l['application_ucp_noaffected'] = 'none affected characters.';
-
+$l['application_ucp_thread_wantedurltitle'] = "Character is a request";
+$l['application_ucp_thread_nowanted'] = "No request";
+$l['application_ucp_thread_trigger'] = "Trigger Warning:";
+$l['application_ucp_wobgroups'] = 'Primary User Group *';
+$l['application_ucp_wobgroups2'] = 'Secondary User Group';
+$l['application_ucp_nonewobgroups2'] = 'No secondary group';
+$l['application_ucp_responsible'] ="<span class=\"aucp_respmod\">Correction taken over by {1}</span>";
+$l['application_ucp_noresponsible'] ="<span class=\"aucp_respmod\">Correction has not yet been taken over</span>";
+$l['application_ucp_wobbtn'] = 'WOB';
+$l['application_ucp_affected_label'] = 'Affected Characters:';
+$l['application_ucp_noaffected'] = 'No other characters affected.';
 
 //My Alert
-$l['myalerts_setting_application_ucp_affected'] = 'personal description: Alert when your character is affected.';
-$l['application_ucp_affected'] = "{1} has published a personal description which affects your character. Please let us know if you're fine with that.";
+$l['myalerts_setting_application_ucp_affected'] = 'Profiles: Notification if a profile concerns you.';
+$l['application_ucp_affected'] = '{1} has posted a profile that concerns you. Please give your approval.';
