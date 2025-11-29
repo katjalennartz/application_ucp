94a6aa7ad8dad9d1ee423f2f39799a7b0a29a848
Katja <47885564+katjalennartz@users.noreply.github.com>
2025-11-29 17:59:54 +0100

    Add files via upload

--- upload/inc/languages/deutsch_du/application_ucp.lang.php
+++ upload/inc/languages/deutsch_du/application_ucp.lang.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Steckbriefe im UCP  1.0 Main Language File
  * by risuena
@@ -8,7 +9,7 @@
 $l['application_ucp_temps_title'] = 'Steckbriefübersicht';
 $l['application_ucp_temps_ready_applications'] = 'fertige Steckbriefe';
 $l['application_ucp_temps_in_correction_user'] = 'Steckbriefe die vom User korrigiert werden müssen';
-
+$l['application_ucp_notready'] = '<br><b>Du hast noch keinen Steckbrief eingereicht.</b>';
 $l['application_ucp_temps_charakter'] = 'Charakter';
 $l['application_ucp_temps_application'] = 'Steckbrief';
 $l['application_ucp_temps_wip'] = 'In Bearbeitung?';
@@ -24,31 +25,43 @@ $l['application_ucp_temps_correctdate'] = 'eingereicht/korrigiert am';
 $l['application_ucp_usernav'] = 'Steckbrief';
 $l['application_ucp_fillapplication'] = 'Steckbrief ausfüllen';
 $l['application_ucp_mandatory'] = '*';
-$l['application_ucp_trigger'] = 'Gebe hier mögliche Trigger für das Profil ein.';
+$l['application_ucp_trigger'] = 'Gebe hier mögliche Inhaltswarnungen für das Profil ein.';
 $l['application_ucp_wanted'] = 'Ist der Charakter ein Gesuch?';
 $l['application_ucp_wanted_url'] = 'Titel des Gesuch, oder Name';
 $l['application_ucp_affected_ucp'] = 'Betroffene Mitglieder';
 $l['application_ucp_extbtn'] = 'Frist Verlängern';
 $l['application_ucp_save'] = 'Speichern';
 $l['application_ucp_readybtn'] = 'Speichern & zur Kontrolle';
-$l['application_ucp_readybtn_prewob'] = 'Zum Pre Wob einreichen';
-
- 
+$l['application_ucp_readybtn_prewob'] = 'Zwischenwob einreichen';
+$l['application_ucp_ucp_modinfo'] = ' Dein Steckbrief wird von {1} korrigiert.';
+$l['application_ucp_ucp_correction'] = ' <span class="need_correction">Dein Steckbrief muss von dir überarbeitet werden. Bei Rückfragen melde dich gerne beim Team.</span>';
+$l['application_ucp_ucp_hasprewob'] = '<br><span class="prewob_yes">Du hast dein Zwischen Wob</span>.';
+$l['application_ucp_ucp_waitingwob'] = '<br><span class="waiting">Du hast deinen Steckbrief zum endgültigen WOB eingereicht</span>.';
 $l['application_ucp_saveerror'] = 'Du kannst deinen Steckbrief nicht einreichen, ehe nicht alle Pflichtfelder ausgefuellt sind. Wenn du neue betroffene Mitglieder eingetragen hattest, musst du diese vor dem Speichern neu eintragen.';
 $l['application_ucp_export'] = 'Steckbrief exportieren';
 $l['application_ucp_infoheader'] = 'Du bist seit dem {1} registriert. Du hast {2} verlängert und Zeit bis zum {3}.';
 $l['application_ucp_correction'] = 'Dein Steckbrief wurde eingereicht.';
+$l['application_ucp_notready'] = '<br><b>Du hast noch keinen Steckbrief eingereicht.</b>';
 
 //Memberprofile
-$l['application_ucp_profile_trigger'] = "Inhaltswarnung:";
+$l['application_ucp_profile_trigger'] = "Inhaltswarnungen:";
 
 //INDEX
 $l['application_ucp_index_extinfo'] = "Du hast {1} Mal verlängert.";
 $l['application_ucp_index_extinfo_deadline'] = "Du hast noch bis zum {1} Zeit deinen Steckbrief zu vervollständigen.{2}";
 $l['application_ucp_index_nomod'] = "Dein Steckbrief wurde noch von keinem Moderator übernommen.";
+$l['application_ucp_index_nomod_wob'] = 'Dein Steckbrief wurde noch nicht übernommen.';
+
 $l['application_ucp_index_token'] = "Dein Steckbrief wurde von {1} übernommen.";
-$l['application_ucp_index_correction'] = "Dein Steckbrief wurde von {1} korrigiert. Du hast für die Korrektur Zeit bis zum {2}.";
+$l['application_ucp_index_tokenwob'] = 'Dein Steckbrief wurde von {1} übernommen und wartet auf sein WoB. Bei Rückfragen melde dich gerne beim Team.';
+$l['application_ucp_index_correction'] = "Für deinen Steckbrief wurde von {1} eine Korrektur angefordert.";
+$l['application_ucp_index_prewobyes'] = 'Du hast dein Zwischen WoB erhalten. Finalisiere deinen Steckbrief und reiche ihn ein.';
+$l['application_ucp_index_mod_not_final'] = '{1} hat den Steckbrief noch nicht zum finalen WOB eingereicht.';
+
+
 $l['application_ucp_index_mod_steckialert'] =  "{1} ist mit dem Steckbrief fertig. <a href=\"misc.php?action=take_application&uid={2}\">Korrektur übernehmen</a>";
+$l['application_ucp_index_mod_steckialert_modturn_prewob'] = '{1} hat den Steckbrief zum Zwischen WoB eingereicht. Du hast ihn übernommen und musst ihn noch korrigieren.';
+
 $l['application_ucp_index_mod_steckialert_modturn'] =  "Du hast den Stecki von {1} übernommen. Du musst ihn noch korrigieren.";
 $l['application_ucp_index_mod_steckialert_userturn'] =  "Du hast den Stecki von {1} übernommen. Der User hat noch nicht korrigiert";
 $l['application_ucp_index_mod_steckialert_userhascorrected'] =  "Du hast den Stecki von {1} übernommen. Der User korrigiert hat und du musst drüberschauen.";
@@ -60,13 +73,11 @@ $l['application_ucp_thread_trigger'] = "Inhaltswarnung:";
 $l['application_ucp_wobgroups'] = 'Primäre Benutzergruppe *';
 $l['application_ucp_wobgroups2'] = 'Sekundäre Benutzergruppe';
 $l['application_ucp_nonewobgroups2'] = 'Keine sekundäre Gruppe';
-$l['application_ucp_responsible'] ="<span class=\"aucp_respmod\">Korrektur übernommen von {1}</span>";
-$l['application_ucp_noresponsible'] ="<span class=\"aucp_respmod\">Korrektur wurde noch nicht übernommen</span>";
+$l['application_ucp_responsible'] = "<span class=\"aucp_respmod\">Korrektur übernommen von {1}</span>";
+$l['application_ucp_noresponsible'] = "<span class=\"aucp_respmod\">Korrektur wurde noch nicht übernommen</span>";
 $l['application_ucp_wobbtn'] = 'WOB';
 $l['application_ucp_affected_label'] = 'Betroffene Charaktere:';
 $l['application_ucp_noaffected'] = 'Keine anderen Charaktere betroffen.';
-
-
 //My Alert
 $l['myalerts_setting_application_ucp_affected'] = 'Steckbriefe: Benachrichtigung wenn dich ein Steckbrief betrifft.';
 $l['application_ucp_affected'] = '{1} hat einen Steckbrief gepostet der dich betrifft. Bitte gib dein Okay.';
